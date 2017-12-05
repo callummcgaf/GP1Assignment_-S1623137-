3377bc6fda7ef0211989cabbede1f638d51e6b4c
Callum McGaffin <callummcgaffin@outlook.com>
2017-12-05 23:14:03 +0000

    User key and mouse input
    Updated the code to contain multiple images to indicate if certain key inputs were made by the user, including key arrow and mouse input.

--- GP1 Assignment (S1623137)/GP1 Assignment (S1623137)/window.cpp
+++ GP1 Assignment (S1623137)/GP1 Assignment (S1623137)/window.cpp
@@ -6,13 +6,32 @@ int main(int argc, char *argv[])
 {
 	// Declare the window to be created and the window surfaces to be used
 	SDL_Window *window = nullptr;
+	SDL_Surface *windowSurface = nullptr;
+	SDL_Surface *staticImage = nullptr;
+	SDL_Surface *upImage = nullptr;
+	SDL_Surface *downImage = nullptr;
+	SDL_Surface *rightImage = nullptr;
+	SDL_Surface *leftImage = nullptr;
+	SDL_Surface *currentImage = nullptr;
+	SDL_Surface *mleftImage = nullptr;
+	SDL_Surface *mrightImage = nullptr;
 
 	// Initialise SDL for video display
 	SDL_Init(SDL_INIT_VIDEO);
 
 	// Then create an application window and display in the centre of the screen
-	window = SDL_CreateWindow("SDL Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+	window = SDL_CreateWindow("SDL Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 344, 344, SDL_WINDOW_SHOWN);
+	windowSurface = SDL_GetWindowSurface(window);
 	
+	staticImage = SDL_LoadBMP("Arrow Keys.bmp");
+	upImage = SDL_LoadBMP("Arrow Keys (UP).bmp");
+	downImage = SDL_LoadBMP("Arrow Keys (Down).bmp");
+	rightImage = SDL_LoadBMP("Arrow Keys (Right).bmp");
+	leftImage = SDL_LoadBMP("Arrow Keys (Left).bmp");
+	mleftImage = SDL_LoadBMP("Mouse (Left).bmp");
+	mrightImage = SDL_LoadBMP("Mouse (Right).bmp");
+	currentImage = staticImage;
+
 	//Set up the game loop
 	bool isRunning = true;
 	// Create a variable to record events input by the user
@@ -29,14 +48,58 @@ int main(int argc, char *argv[])
 			{
 				isRunning = false;
 			}
+
+			else if (ev.type == SDL_KEYDOWN)
+			{
+				switch (ev.key.keysym.sym)
+				{
+				case SDLK_UP:
+					currentImage = upImage;
+					break;
+				case SDLK_DOWN:
+					currentImage = downImage;
+					break;
+				case SDLK_RIGHT:
+					currentImage = rightImage;
+					break;
+				case SDLK_LEFT:
+					currentImage = leftImage;
+					break;
+				}
+			}
+			else if (ev.type == SDL_MOUSEBUTTONDOWN)
+			{
+				if (ev.button.button == SDL_BUTTON_LEFT)
+					currentImage = mleftImage;
+				else if (ev.button.button == SDL_BUTTON_RIGHT)
+					currentImage = mrightImage;
+			}
+			else
+			{
+				currentImage = staticImage;
+			}
 			
+			SDL_BlitSurface(currentImage, NULL, windowSurface, NULL);
+
 			// If the user has not quit the application yet, then update the application window
 			SDL_UpdateWindowSurface(window);
 		}
 	}
 
+	SDL_FreeSurface(staticImage);
+	SDL_FreeSurface(upImage);
+	SDL_FreeSurface(downImage);
+	SDL_FreeSurface(rightImage);
+	SDL_FreeSurface(leftImage);
+	SDL_FreeSurface(mleftImage);
+	SDL_FreeSurface(mrightImage);
+
 	// Destroy the application window and Quit the console
 	SDL_DestroyWindow(window);
+
+	currentImage = staticImage = upImage = downImage = rightImage = leftImage = mleftImage = mrightImage = nullptr;
+	window = nullptr;
+
 	SDL_Quit();
 
 	return 0;
