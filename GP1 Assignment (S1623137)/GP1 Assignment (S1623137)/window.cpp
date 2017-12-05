fc595f9da45b239b9aa7a0fe24452f7964af61bc
Callum McGaffin <callummcgaffin@outlook.com>
2017-12-05 16:47:13 +0000

    Events testing
    Deleted the image loading routines to be replaced with a game loop to record events and close the application properly if the user has pressed the quit button on the application window.

--- GP1 Assignment (S1623137)/GP1 Assignment (S1623137)/window.cpp
+++ GP1 Assignment (S1623137)/GP1 Assignment (S1623137)/window.cpp
@@ -6,53 +6,37 @@ int main(int argc, char *argv[])
 {
 	// Declare the window to be created and the window surfaces to be used
 	SDL_Window *window = nullptr;
-	SDL_Surface *windowSurface = nullptr;
-	SDL_Surface *imageSurface = nullptr;
 
 	// Initialise SDL for video display
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	SDL_Init(SDL_INIT_VIDEO);
+
+	// Then create an application window and display in the centre of the screen
+	window = SDL_CreateWindow("SDL Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+	
+	//Set up the game loop
+	bool isRunning = true;
+	// Create a variable to record events input by the user
+	SDL_Event ev;
+
+	// While the boolean 'isRunning' is equal to true
+	while (isRunning)
 	{
-		std::cout << "Video Initialisation Error: " << SDL_GetError() << std::endl;
-	}
-	// If initialisation returns with no errors
-	else
-	{
-		// Then create an application window and display in the centre of the screen
-		window = SDL_CreateWindow("SDL Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
-		// If there is no window then display error to the console
-		if (window == NULL)
+		// And While the events given by the user are not equal to 0
+		while (SDL_PollEvent(&ev) != 0)
 		{
-			std::cout << "Window creation error: " << SDL_GetError() << std::endl;
-		}
-		// Else, Pass windowSurface on to the application window to display and load the image for the image surface to use
-		else
-		{
-			windowSurface = SDL_GetWindowSurface(window);
-			imageSurface = SDL_LoadBMP("hello_world.bmp");
-			// If there is no image of the specified name, then return an error message to the console
-			if (imageSurface == NULL)
+			// Check if the user has clicked the quit application button, if they have then set the variable 'isRunning' to false, causing the game loop to end
+			if (ev.type == SDL_QUIT)
 			{
-				std::cout << "Image could not be loaded, Loading Error: " << SDL_GetError() << std::endl;
-			}
-			// If image was successfully loaded, then merge windowSurface with ImageSurface to display the image in the application window on screen
-			else
-			{
-				SDL_BlitSurface(imageSurface, NULL, windowSurface, NULL);
-				SDL_UpdateWindowSurface(window);
-				// Wait for 2 seconds
-				SDL_Delay(2000);
+				isRunning = false;
 			}
 			
+			// If the user has not quit the application yet, then update the application window
+			SDL_UpdateWindowSurface(window);
 		}
 	}
 
-	// Free the media in the image surface as not to cause a memory leak
-	SDL_FreeSurface(imageSurface);
-	imageSurface = nullptr;
-	// After the image has been displayed for 2 Seconds, reset the window and windowSurface, destroy the application window and quit the console 
+	// Destroy the application window and Quit the console
 	SDL_DestroyWindow(window);
-	window = nullptr;
-	windowSurface = nullptr;
 	SDL_Quit();
 
 	return 0;
